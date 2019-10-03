179f62d9e99677d137a32fdb8e31118c7fc2e2ae
Richard Anabeto Opoku <anabeto93@gmail.com>
2019-10-03 21:45:22 +0000

    started with creating the menus using VueJS. Current hack is to pull the menus from the HomeController and pass it to the menu-container component

---
+++ resources/js/modules/menu/MenuContainer.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="wrapper menu__container">
+        <p>This is the menu container</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['items']
+}
+</script>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                <menu-container :items="{{ json_encode($menus) }}"></menu-container>
                 </div>
             </div>
         </div>
